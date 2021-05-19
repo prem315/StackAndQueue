43aa7338d44e4ce5caebc2c6d1eb86336cdd6447
prem315 <prem.patel3105@gmail.com>
2021-05-19 21:57:42 +0530

    reversing first k ele of queue

---
+++ challenge3/challenge3.js
@@ -0,0 +1,42 @@
+// Reversing First k Elements of Queue
+"use strict";
+const Queue = require('./queue.js');
+const Stack = require('./stack.js');
+
+function reverseKElementsOfQueue(queue, k) {
+    // craete an empty stack
+    let stack = new Stack();
+    if(queue.isEmpty() !== true) {
+
+        for(let i = 0; i < k; i++) {
+            // remove first k ele from queue and push it to stack
+            let ele = queue.dequeue();
+            // push ele to stack
+            stack.push(ele);
+        }
+
+        // empty stack and push it to queue
+        while(stack.isEmpty() !== true) {
+            // remove ele from stack
+            let ele = stack.pop();
+            // push(enqueue) it to queue
+            queue.enqueue(ele);
+        }
+
+        // remove first k ele from queue and push(enqueue) it to queue 
+        for(let i = 0; i < (queue.size() - k); i++) {
+            let ele = queue.dequeue();
+            queue.enqueue(ele)
+        }
+    }
+
+    console.log(queue);
+    console.log(stack);
+}
+
+let queue = new Queue()
+for(let i = 0; i < 10; i++) {
+    queue.enqueue(i+1);
+}
+
+reverseKElementsOfQueue(queue, 4) 
---
+++ challenge3/queue.js
@@ -0,0 +1,35 @@
+module.exports = class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    getFront() {
+        if(this.isEmpty()) {
+            return null;
+        } else {
+            return this.items[0];
+        }
+    }
+
+    size() {
+        return this.items.length;
+    }
+
+    enqueue(element) {
+        this.items.push(element);
+    }
+
+    dequeue() {
+        if(this.isEmpty()) {
+            return null;
+        } else {
+            return this.items.shift()
+        }
+    }
+}
+
+
